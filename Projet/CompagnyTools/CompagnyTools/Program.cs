dfda153e502de26124e54011a5efe78e41c38ef5
Djangoo123 <142230327+Djangoo123@users.noreply.github.com>
2023-08-22 16:27:26 +0200

    Delete useless code

--- Projet/CompagnyTools/CompagnyTools/Program.cs
+++ Projet/CompagnyTools/CompagnyTools/Program.cs
@@ -11,28 +11,8 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         builder.Services.AddControllersWithViews();
-
-        IConfigurationRoot configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true)
-            .Build();
-
-        string dbConnectionString = "Server=localhost;Port=5432;Database=;User Id=;Password=";
-
-        // Launch connection postgres 
-        ServiceProvider serviceProvider = new ServiceCollection()
-        .AddDbContext<EFCoreContext>()
-        .AddDbContext<Access>(
-            options => options.UseNpgsql(dbConnectionString)
-        )
-        .AddSingleton<IConfiguration>(configuration)
-        .BuildServiceProvider();
-
         CreateWebHostBuilder(args).Build().Run();
-
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>

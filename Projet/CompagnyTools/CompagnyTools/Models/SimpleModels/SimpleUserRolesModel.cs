ca110855a81bf2e6229d37f19081f46c5e3e38ec
Djangoo123 <142230327+Djangoo123@users.noreply.github.com>
2025-11-28 15:05:40 +0100

    Remove CompagnyTools project and move other poc to appropriate folder

--- Projet/CompagnyTools/CompagnyTools/Models/SimpleModels/SimpleUserRolesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using DAL.Entities;
-
-namespace CompagnyTools.Models.SimpleModels
-{
-    public class SimpleUserRolesModel
-    {
-        public string UserRight { get; set; } = null!;
-    }
-}

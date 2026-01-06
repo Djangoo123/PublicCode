14072dbc2bea0ae1246a7c75e1854ac1d40d7411
Djangoo123 <142230327+Djangoo123@users.noreply.github.com>
2026-01-06 09:13:48 +0100

    Start working on UI ( Blazor )

--- Projet/PursuitSim/PursuitSim_v2/PursuitSim_v2.Web/Program.cs
+++ Projet/PursuitSim/PursuitSim_v2/PursuitSim_v2.Web/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using PursuitSim_v2.Web.Data;
+using PursuitSim_v2.Blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<SimulationService>();
 
 var app = builder.Build();
 
---
+++ Projet/PursuitSim/PursuitSim_v2/PursuitSim_v2.Web/Services/SimulationService.cs
@@ -0,0 +1,28 @@
+﻿using PursuitSim_v2.Core.DroneSim;
+
+namespace PursuitSim_v2.Blazor.Services;
+
+public sealed class SimulationService
+{
+    public bool IsRunning { get; private set; }
+
+    public event Action<string>? OnLog;
+
+    public async Task RunDroneSimAsync(Scenario scenario)
+    {
+        if (IsRunning)
+            return;
+
+        IsRunning = true;
+        OnLog?.Invoke("Simulation started");
+
+        var sim = new Simulation(scenario, "ui");
+
+        sim.LogMessage += msg => OnLog?.Invoke(msg);
+
+        await Task.Run(sim.Run);
+
+        OnLog?.Invoke("Simulation finished");
+        IsRunning = false;
+    }
+}

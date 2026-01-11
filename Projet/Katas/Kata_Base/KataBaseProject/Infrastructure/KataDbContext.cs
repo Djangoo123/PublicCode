e9ecd1db84edad3768edd4fe64de2c69091acc4e
Djangoo123 <142230327+Djangoo123@users.noreply.github.com>
2026-01-11 16:46:44 +0100

    Add postgresql img ( docker ) to kata base prokect

---
+++ Projet/Katas/Kata_Base/KataBaseProject/Infrastructure/KataDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KataBaseProject.Infrastructure
+{
+    public class KataDbContext : DbContext
+    {
+        public KataDbContext(DbContextOptions<KataDbContext> options)
+            : base(options) { }
+    }
+
+}
--- Projet/Katas/Kata_Base/KataBaseProject/Program.cs
+++ Projet/Katas/Kata_Base/KataBaseProject/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KataBaseProject.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<KataDbContext>(options =>
+    options.UseNpgsql(
+        "Host=localhost;Port=5432;Database=kata_db;Username=kata;Password=kata"
+    ));
+
+using var provider = services.BuildServiceProvider();
+
+using var scope = provider.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<KataDbContext>();
+
+db.Database.Migrate();
